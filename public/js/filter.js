eee0684f42ded3ba26ed8a8e7a5355ce321ce343
Mitul Patel <mitulpatel.hsd@gmail.com>
2016-05-22 23:37:17 -0700

    Update filter.js

--- public/js/filter.js
+++ public/js/filter.js
@@ -25,9 +25,7 @@ shoppingCart.filter('changeThumbURL', function() {
 shoppingCart.filter('setDecimal', function ($filter) {
     return function (input, places, quantity) {
         if (isNaN(input)) return (input * quantity);
-        // If we want 1 decimal place, we want to mult/div by 10
-        // If we want 2 decimal places, we want to mult/div by 100, etc
-        // So use the following to create that factor
+
         var factor = "1" + Array(+(places > 0 && places + 1)).join("0");
         return Math.round(input * factor * quantity)/factor;
     };
